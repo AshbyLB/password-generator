c28f4e6855a7ba07a701a35b1d566762acaf6fb8
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-08-20 16:23:34 -0400

    Function var updates

--- script.js
+++ script.js
@@ -4,67 +4,69 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  var passChoice = prompt("How many characteers would you like the password to be.  Please choose between 8 and 128 characters in length.");
-  if (passChoice < 8 || passChoice > 128) {
-    passChoice = prompt("Please choose between 8 and 128 characters.");
-}
-
-  
-  var userUpperCase = confirm("Would you like uppercase letters?");
-  console.log(userUpperCase);
-  
-  var userLowerCase = confirm("Would you like lowercase letters?");
-  console.log(userLowerCase);
-  
-  var userNumbers = confirm("Would you like numbers?");
-  console.log(userNumbers);
-  
-  var userSpecial = confirm("Would you like special characters?");
-  console.log(userSpecial);
-  
   var numbers = "0123456789";
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerCase = upperCase.toLowerCase();
   var special = "!@#$%^&*()_+~";
-  
   var value = [];
 
   var possOutcome = "";
 
+  var password = "";
+
+
+
+
+
+  var passChoice = prompt("How many characteers would you like the password to be.  Please choose between 8 and 128 characters in length.");
+  if (passChoice < 9 || passChoice > 128) {
+    passChoice = prompt("Please choose between 8 and 128 characters.");
+  }
+
+
+  var userUpperCase = confirm("Would you like uppercase letters?");
   if (userUpperCase) {
     var randomChoice = Math.floor(Math.random() * upperCase.length);
     var upperChoice = upperCase[randomChoice];
     value.push(upperChoice);
-    possOutcome = possOutcome.concat(upperCase)
+    possOutcome = possOutcome.concat(upperCase);
   }
- 
+
+
+  var userLowerCase = confirm("Would you like lowercase letters?");
+
   if (userLowerCase) {
     var randomChoice = Math.floor(Math.random() * lowerCase.length);
     var lowerChoice = lowerCase[randomChoice];
     value.push(lowerChoice);
-    possOutcome = possOutcome.concat(lowerCase)
+    possOutcome = possOutcome.concat(lowerCase);
   }
-  
+
+  var userNumbers = confirm("Would you like numbers?");
   if (userNumbers) {
     var randomChoice = Math.floor(Math.random() * numbers.length);
     var numChoice = numbers[randomChoice];
     value.push(numChoice);
-    possOutcome = possOutcome.concat(numbers)
+    possOutcome = possOutcome.concat(numbers);
   }
 
+
+  var userSpecial = confirm("Would you like special characters?");
   if (userSpecial) {
     var randomChoice = Math.floor(Math.random() * special.length);
     var specialChoice = special[randomChoice];
     value.push(specialChoice);
-    possOutcome = possOutcome.concat(special)
+    possOutcome = possOutcome.concat(special);
   }
-  
-  for (var i = 0; i < passChoice.length; i++) {
 
-
+  for (var i = 0; i < passChoice - value.length; i++) {
+    var randomChoice = Math.floor(Math.random() * passChoice.length);
+    password += passChoice[randomChoice];
   }
-  
-  return "FUN TIMES!";
+
+  password += value.join('');
+
+  return password;
 }
 // Write password to the #password input
 function writePassword() {
@@ -72,7 +74,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   if (password) {
-  passwordText.value = password;
+    passwordText.value = password;
   }
 }
 
