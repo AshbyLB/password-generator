0361e837fe84064e1ff15a89d1171672bbf3f02c
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-08-20 09:59:08 -0400

    Trying to add initial length prompt

--- script.js
+++ script.js
@@ -1,55 +1,69 @@
 
 var generateBtn = document.querySelector("#generate");
 
-var userUpperCase = confirm("Would you like uppercase letters?");
-console.log(userUpperCase);
-
-var userLowerCase = confirm("Would you like lowercase letters?");
-console.log(userLowerCase);
-
-var userNumbers = confirm("Would you like numbers?");
-console.log(userNumbers);
-
-var userSpecial = confirm("Would you like special characters?");
-console.log(userSpecial);
-
-var numbers = "0123456789";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = upperCase.toLowerCase();
-var special = "!@#$%^&*()_+~";
-
-var value = [];
-
 
 function generatePassword() {
 
+  var passChoose = prompt("How many characteers would you like the password to be.  Please choose between 8 and 128 characters in length.");
+  if (passChoice < 8 || passChoice > 128) {
+    passChoice = prompt("Please choose between 8 and 128 characters.");
+    return generatePassword();
+  }
+
+  
+  var userUpperCase = confirm("Would you like uppercase letters?");
+  console.log(userUpperCase);
+  
+  var userLowerCase = confirm("Would you like lowercase letters?");
+  console.log(userLowerCase);
+  
+  var userNumbers = confirm("Would you like numbers?");
+  console.log(userNumbers);
+  
+  var userSpecial = confirm("Would you like special characters?");
+  console.log(userSpecial);
+  
+  var numbers = "0123456789";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = upperCase.toLowerCase();
+  var special = "!@#$%^&*()_+~";
+  
+  var value = [];
+
+
+
   if (userUpperCase) {
-    var randomUp = Math.floor(Math.random() * upperCase.length);
-    var upperChoice = upperCase[randomUp];
+    var randomChoice = Math.floor(Math.random() * upperCase.length);
+    var upperChoice = upperCase[randomChoice];
     value.push(upperChoice);
     console.log(upperChoice);
   }
   
   if (userLowerCase) {
-    var randomLow = Math.floor(Math.random() * lowerCase.length);
-    var lowerChoice = lowerCase[randomlow];
+    var randomChoice = Math.floor(Math.random() * lowerCase.length);
+    var lowerChoice = lowerCase[randomChoice];
     value.push(lowerChoice);
     console.log(lowerChoice);
   }
   
   if (userNumbers) {
-    var randomNum = Math.floor(Math.random() * numbers.length);
-    var numChoice = numbers[randomNum];
+    var randomChoice = Math.floor(Math.random() * numbers.length);
+    var numChoice = numbers[randomChoice];
     value.push(numChoice);
     console.log(numChoice);
   }
   
   if (userSpecial) {
-    var randomSpe = Math.floor(Math.random() * special.length);
-    var specialChoice = special[randomSpe];
+    var randomChoice = Math.floor(Math.random() * special.length);
+    var specialChoice = special[randomChoice];
     value.push(specialChoice);
     console.log(specialChoice);
   }
+  
+  for (var i = 0; i < randomChoice.length; i++){
+
+  }
+  
   return "FUN TIMES!";
 }
 // Write password to the #password input
